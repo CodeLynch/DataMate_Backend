41e16fdbfd53bf1c286e1a8ad5cd70041014064b
Vergil <88496530+CodeLynch@users.noreply.github.com>
2023-11-15 21:59:46 +0800

    for deployment test

--- src/main/java/com/capstone/datamate/Controller/JDBCController.java
+++ src/main/java/com/capstone/datamate/Controller/JDBCController.java
@@ -8,14 +8,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capstone.datamate.Config.JdbcTemplateImpl;
+import com.capstone.datamate.Entity.SqlRequestEntity;
 
 @CrossOrigin("http://localhost:3000/")
 @RestController
 public class JDBCController {
     @Autowired
     JdbcTemplateImpl jdbc;
+    // @PostMapping("/convert")
+    // public void executeSQL(@RequestParam String tblName, @RequestParam String vals, @RequestParam int op){
+    //     jdbc.executeSQL(tblName, vals, op);
+    // }
+
     @PostMapping("/convert")
-    public void executeSQL(@RequestParam String tblName, @RequestParam String vals, @RequestParam int op){
-        jdbc.executeSQL(tblName, vals, op);
+    public void executeSQL(@RequestBody SqlRequestEntity sql){
+        jdbc.executeSQL(sql.getTblName(), sql.getVals(), sql.getOperation());
     }
 }
