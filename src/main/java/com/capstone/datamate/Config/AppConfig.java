c7c463b10738cefbfb8a773ee11a58d45acd8320
johnvergil.aparri <johnvergil.aparri@cit.edu>
2023-11-16 22:53:01 +0800

    Fixed encryption

--- src/main/java/com/capstone/datamate/Config/AppConfig.java
+++ src/main/java/com/capstone/datamate/Config/AppConfig.java
@@ -2,6 +2,7 @@ package com.capstone.datamate.Config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,14 +12,21 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 @Configuration
 @ComponentScan("com.capstone.datamate")
 public class AppConfig {
-
+ 
+    @Value("${DB_USERNAME}")
+    private String user;
+    
+    @Value("${DB_PASSWORD}")
+    private String pass;
+        
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        
         dataSource.setDriverClassName("org.postgresql.Driver");
         dataSource.setUrl("jdbc:postgresql://dpg-clacpn9m6hds73dhnba0-a.oregon-postgres.render.com/db_datamate");
-        dataSource.setUsername("db_datamate_user");
-        dataSource.setPassword("Oj9iHI28hYp5nEcmwwvouxw6bTaIdFZA");
+        dataSource.setUsername(user);
+        dataSource.setPassword(pass);
 
         return dataSource;
     }
